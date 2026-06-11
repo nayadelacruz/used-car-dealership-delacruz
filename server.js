3f614673cdbf42f4f9afea5984515e4c1221fd76
nayadelacruz <nayadelacruz@gmail.com>
2026-06-11 16:28:33 -0700

    created ejs files and created routes

--- server.js
+++ server.js
@@ -1,25 +1,51 @@
 // Imports
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const name = process.env.NAME; // <-- NEW
 
-app.get('/', (req, res) => {
-    res.send(`Welcome, ${name}!`); // <-- UPDATED
-});
+//const name = process.env.NAME; // <-- NEW
 
 /**
  * Declare Important Variables
  */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
-
 /**
  * Setup Express Server
  */
 const app = express();
 
 /**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
  * Declare Routes
  */
+app.get('/', (req, res) => {
+    const title = 'Welcome Home';
+    res.render('home', { title });
+});
+app.get('/categories', (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
+app.get('/details', (req, res) => {
+    const title = 'Our Products';
+    res.render('details', { title });
+});
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {

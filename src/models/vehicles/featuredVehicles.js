dbe5da370efc20b45327001eb0a6d03cfa87b507
nayadelacruz <nayadelacruz@gmail.com>
2026-06-25 13:30:35 -0700

    modified the model for the featuredVehicle feature featuredVehicles.js to match changes made in the database

--- src/models/vehicles/featuredVehicles.js
+++ src/models/vehicles/featuredVehicles.js
@@ -2,10 +2,16 @@ import db from '../db.js';
 
 const getFeaturedVehicles = async () => {
     const result = await db.query(`
-        SELECT *
+        SELECT 
+            vehicles_details.*,
+            vehicle_images.image_url AS vehicle_image_url
+            vehicle_images.alt_text AS vehicle_image_alt
         FROM vehicles_details
-        WHERE featured = TRUE
-        AND availability = TRUE
+        JOIN vehicle_images
+            ON vehicles_details.vehicle_id = vehicle_images.vehicle_id
+        WHERE vehicles_details.featured = TRUE
+        AND vehicles_details.availability = TRUE
+        AND vehicle_images.is_primary = TRUE
         LIMIT 3
     `);
 
